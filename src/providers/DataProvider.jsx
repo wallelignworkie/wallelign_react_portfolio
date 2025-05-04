2881bccb0774965689dfe71e57500a454062a7e1
wallelign <walleworkieas@gmail.com>
2025-05-04 19:24:07 +0300

    updates

--- src/providers/DataProvider.jsx
+++ src/providers/DataProvider.jsx
@@ -1,160 +1,162 @@
-import React, {createContext, useContext, useEffect, useState} from 'react'
-import {useUtils} from "/src/helpers/utils.js"
-import {useEmails} from "/src/helpers/emails.js"
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { useUtils } from "/src/helpers/utils.js";
+import { useEmails } from "/src/helpers/emails.js";
 
-const DataContext = createContext(null)
-export const useData = () => useContext(DataContext)
+const DataContext = createContext(null);
+export const useData = () => useContext(DataContext);
 
 const Status = {
-    NOT_LOADED: 0,
-    LOADED: 1
-}
+  NOT_LOADED: 0,
+  LOADED: 1,
+};
 
-export const DataProvider = ({children}) => {
-    const utils = useUtils()
-    const emails = useEmails()
+// data Provider
+export const DataProvider = ({ children }) => {
+  const utils = useUtils();
+  const emails = useEmails();
 
-    const [status, setStatus] = useState(Status.NOT_LOADED)
-    const [jsonData, setJsonData] = useState({
-        settings: {},
-        strings: {},
-        sections: [],
-        categories: []
-    })
-    const [dataValidated, setDataValidated] = useState(false)
+  const [status, setStatus] = useState(Status.NOT_LOADED);
+  const [jsonData, setJsonData] = useState({
+    settings: {},
+    strings: {},
+    sections: [],
+    categories: [],
+  });
+  const [dataValidated, setDataValidated] = useState(false);
 
-    useEffect(() => {
-        _load().then(r => {})
-    }, [])
+  useEffect(() => {
+    _load().then((r) => {});
+  }, []);
 
-    useEffect(() => {
-        if(!jsonData.settings)
-            return
+  useEffect(() => {
+    if (!jsonData.settings) return;
 
-        if(!dataValidated) {
-            _validate()
-        }
-
-        if(jsonData.settings['emailjs']) {
-            emails.init(jsonData.settings['emailjs'])
-        }
-    }, [jsonData])
-
-    const _load = async () => {
-        const jSettings = await _loadJson("/data/settings.json")
-        const jStrings = await _loadJson("/data/strings.json")
-        const jStructure = await _loadJson("/data/structure.json")
-
-        const categories = jStructure["categories"]
-
-        const sections = jStructure["sections"]
-        for(const section of sections) {
-            const category = categories.find(category => category.id === section["categoryId"])
-            if(!category) {
-                throw new Error(`[DataProvider] The section with id "${section.id}" has an invalid categoryId "${section["categoryId"]}". There's no such category.`)
-            }
-
-            section.category = category
-            section.content = await _loadJson(section["jsonPath"])
-        }
-
-        const filteredCategories = categories.filter(category => {
-            return sections.find(section => section.categoryId === category.id)
-        })
-
-        setJsonData(prevState => ({
-            ...prevState,
-            settings: jSettings,
-            strings: jStrings,
-            categories: filteredCategories,
-            sections: jStructure["sections"]
-        }))
-
-        setStatus(Status.LOADED)
+    if (!dataValidated) {
+      _validate();
     }
 
-    const _validate = () => {
-        const sections = jsonData.sections
-        if(!sections.length) {
-            return
-        }
+    if (jsonData.settings["emailjs"]) {
+      emails.init(jsonData.settings["emailjs"]);
+    }
+  }, [jsonData]);
 
-        setDataValidated(true)
+  const _load = async () => {
+    const jSettings = await _loadJson("/data/settings.json");
+    const jStrings = await _loadJson("/data/strings.json");
+    const jStructure = await _loadJson("/data/structure.json");
+
+    const categories = jStructure["categories"];
+
+    const sections = jStructure["sections"];
+    for (const section of sections) {
+      const category = categories.find(
+        (category) => category.id === section["categoryId"]
+      );
+      if (!category) {
+        throw new Error(
+          `[DataProvider] The section with id "${section.id}" has an invalid categoryId "${section["categoryId"]}". There's no such category.`
+        );
+      }
+
+      section.category = category;
+      section.content = await _loadJson(section["jsonPath"]);
     }
 
-    const _loadJson = async (path) => {
-        const actualPath = utils.resolvePath(path)
-        const request = await fetch(actualPath)
-        return request.json()
+    const filteredCategories = categories.filter((category) => {
+      return sections.find((section) => section.categoryId === category.id);
+    });
+
+    setJsonData((prevState) => ({
+      ...prevState,
+      settings: jSettings,
+      strings: jStrings,
+      categories: filteredCategories,
+      sections: jStructure["sections"],
+    }));
+
+    setStatus(Status.LOADED);
+  };
+
+  const _validate = () => {
+    const sections = jsonData.sections;
+    if (!sections.length) {
+      return;
     }
 
-    const getSettings = () => {
-        return jsonData.settings
-    }
+    setDataValidated(true);
+  };
 
-    const getStrings = () => {
-        return jsonData.strings
-    }
+  const _loadJson = async (path) => {
+    const actualPath = utils.resolvePath(path);
+    const request = await fetch(actualPath);
+    return request.json();
+  };
 
-    const getSections = () => {
-        return jsonData.sections
-    }
+  const getSettings = () => {
+    return jsonData.settings;
+  };
 
-    const getCategories = () => {
-        return jsonData.categories
-    }
+  const getStrings = () => {
+    return jsonData.strings;
+  };
 
-    const getCategorySections = (category) => {
-        if(!category)
-            return []
-        return jsonData.sections.filter(section => section["categoryId"] === category.id)
-    }
+  const getSections = () => {
+    return jsonData.sections;
+  };
 
-    const listImagesForCache = () => {
-        const settings = getSettings()
-        const sections = getSections()
+  const getCategories = () => {
+    return jsonData.categories;
+  };
 
-        const images = [
-            utils.resolvePath(settings.profile['logoUrl']),
-            utils.resolvePath(settings.profile['profilePictureUrl'])
-        ]
+  const getCategorySections = (category) => {
+    if (!category) return [];
+    return jsonData.sections.filter(
+      (section) => section["categoryId"] === category.id
+    );
+  };
 
-        settings['supportedLanguages'].forEach(lang => {
-            images.push(utils.resolvePath(lang['flagUrl']))
-        })
+  const listImagesForCache = () => {
+    const settings = getSettings();
+    const sections = getSections();
 
-        sections.forEach(section => {
-            if(!section.content || !section.content.articles)
-                return
+    const images = [
+      utils.resolvePath(settings.profile["logoUrl"]),
+      utils.resolvePath(settings.profile["profilePictureUrl"]),
+    ];
 
-            section.content.articles.forEach(article => {
-                if(!article.items)
-                    return
+    settings["supportedLanguages"].forEach((lang) => {
+      images.push(utils.resolvePath(lang["flagUrl"]));
+    });
 
-                article.items.forEach(item => {
-                    if(!item.icon || !item.icon.img)
-                        return
+    sections.forEach((section) => {
+      if (!section.content || !section.content.articles) return;
 
-                    images.push(utils.resolvePath(item.icon.img))
-                })
-            })
-        })
+      section.content.articles.forEach((article) => {
+        if (!article.items) return;
 
-        return images
-    }
+        article.items.forEach((item) => {
+          if (!item.icon || !item.icon.img) return;
 
-    return (
-        <DataContext.Provider value={{
-            getSettings,
-            getStrings,
-            getSections,
-            getCategories,
-            getCategorySections,
-            listImagesForCache
-        }}>
-            {status === Status.LOADED && dataValidated && (
-                <>{children}</>
-            )}
-        </DataContext.Provider>
-    )
-}
+          images.push(utils.resolvePath(item.icon.img));
+        });
+      });
+    });
+
+    return images;
+  };
+
+  return (
+    <DataContext.Provider
+      value={{
+        getSettings,
+        getStrings,
+        getSections,
+        getCategories,
+        getCategorySections,
+        listImagesForCache,
+      }}
+    >
+      {status === Status.LOADED && dataValidated && <>{children}</>}
+    </DataContext.Provider>
+  );
+};
